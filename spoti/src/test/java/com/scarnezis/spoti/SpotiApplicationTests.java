7a710644bc8a8e66ba3c8aecc4a52c7457b0dd94
SantiagoCarnezis <scarnezis@frba.utn.edu.ar>
2022-08-30 00:04:58 -0300

    First commit

---
+++ spoti/src/test/java/com/scarnezis/spoti/SpotiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scarnezis.spoti;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpotiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
